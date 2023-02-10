83b29ea0c589787db28aef67eb004fb45fdf955e
Conjuction <suhinin-d@mail.ru>
2023-02-10 17:37:55 +0500

    added data created Java Faker

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -1,22 +1,53 @@
 package test;
 
+import com.github.javafaker.Faker;
 import pages.RegistrationPage;
 
+
 public class TestBase {
-    String firstName = "test";
-    String lastName = "test";
-    String userEmail = "test@mail.ru";
-    String gender = "Male";
-    String userNumber = "1234567890";
-    String day = "30";
-    String month = "November";
-    String year = "1987";
-    String subjects = "Maths";
-    String hobbies = "Sports";
-    String currentAddress = "Aqtobe";
-    String state = "NCR";
-    String city = "Delhi";
-    String dayOfBirth = "30 November,1987";
+    Faker faker = new Faker();
+    String firstName = faker.name().firstName();
+    String lastName = faker.name().lastName();
+    String userEmail = faker.internet().emailAddress();
+    String gender = getRandomValue("Male", "Female", "Other");
+
+    String userNumber = String.valueOf(faker.number().numberBetween(0000000000L, 9999999999L));
+    String day = String.format("%02d", faker.number().numberBetween(1, 28));
+    String month = faker.options().option("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+    String year = String.valueOf(faker.number().numberBetween(1900, 2022));
+    String subjects = getRandomValue("Math", "Chemistry", "Physics", "Computer Science", "English", "History");
+    String hobbies = getRandomValue("Sports", "Reading", "Music");
+    String currentAddress = faker.address().fullAddress();
+    String state = getRandomValue("NCR", "Uttar Pradesh", "Haryana", "Rajasthan");
+    String city = getCity();
+    String dayOfBirth = day + " " + month + "," + year;
     String fileName = "avatar.jpg";
     RegistrationPage registrationPage = new RegistrationPage();
+
+    private String getRandomValue(String... initialValues) {
+
+        return faker.options().option(initialValues);
+    }
+
+    private String getCity() {
+        String cityItem = null;
+
+        if (state.equals("NCR")) {
+            cityItem = getRandomValue("Delhi", "Gurgaon", "Noida");
+        }
+
+        if (state.equals("Uttar Pradesh")) {
+            cityItem = getRandomValue("Agra", "Lucknow", "Merrut");
+        }
+
+        if (state.equals("Haryana")) {
+            cityItem = getRandomValue("Karnal", "Panipat");
+        }
+
+        if (state.equals("Rajasthan")) {
+            cityItem = getRandomValue("Jaipur", "Jaiselmer");
+        }
+
+        return cityItem;
+    }
 }
